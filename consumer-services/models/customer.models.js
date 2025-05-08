9643638025f63e1c27d1a97c958e22e210a1ed3e
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-09 04:10:07 +0530

    ADD: customer and order Models

---
+++ consumer-services/models/customer.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema({
+    name:{
+        type: String, 
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    phone:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    address:{
+        type: String,
+        required: true,
+        trim: true
+    }
+},{timestamps: true})
+
+const Customer = mongoose.model("Customer", customerSchema);
+export default Customer;
