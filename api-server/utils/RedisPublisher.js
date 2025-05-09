b9b349abbe37d2fc6d87da587c8468db6e5bc91d
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-09 06:12:59 +0530

    ADD: Send to redis funtion

---
+++ api-server/utils/RedisPublisher.js
@@ -0,0 +1,16 @@
+import client from "./RedisClient";
+
+const publishToStream = async (streamName , data) => {
+    try {
+        const messageData = Object.entries(data).flat();
+
+        const messageId = await client.xAdd(streamName, '*', messageData);
+        console.log(`Message published to stream ${streamName} with ID ${messageId}`);
+    } catch (error) {
+        console.log(`Error publishing message to stream ${streamName}:`, error);
+        throw error
+        
+    }
+}
+
+export default publishToStream
