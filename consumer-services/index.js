d78520d1953418f64cf2694ef8eda0d3857ba880
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-09 23:53:14 +0530

    ADD: Completed index.js

--- consumer-services/index.js
+++ consumer-services/index.js
@@ -1,8 +1,29 @@
 // 1. Start consumer
 // 2. Connect to Redis
 // 3. Connect to MongoDB
-// 4. Loop:
-//    - Listen for new messages on Redis Stream
-//    - Parse message (check if it's customer or order)
-//    - Save to MongoDB using the appropriate model
-//    - Acknowledge message (optional with stream groups)
+// 4. Start order and customer streams
+import {connectRedis} from "./utils/RedisClient";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { startOrderStream } from "./consumers/orderHandler.js";
+import { startCustomerStream } from "./consumers/customerHandler.js";
+dotenv.config();
+
+
+const start = async () => {
+    try {
+        await connectDB();
+        await connectRedis();
+
+        startOrderStream();
+        startCustomerStream();
+        console.log("Consumer services started successfully");
+        
+
+    } catch (error) {
+        console.error("Error starting consumer:", error);
+        process.exit(1);
+    }
+}
+
+start();
