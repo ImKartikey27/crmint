935d0f521bb9a7f0fce4e5a10a726fffba8e82bf
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-10 19:36:13 +0530

    ADD: DB connection

--- backend/api-server/index.js
+++ backend/api-server/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { app } from './app.js';
+import connectDB from '../consumer-services/db/index.js';
 
 
 dotenv.config({
@@ -8,6 +9,12 @@ dotenv.config({
 
 const PORT = process.env.PORT 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+connectDB()
+.then(() => {
+    app.listen(PORT , () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error" , err);
 })
