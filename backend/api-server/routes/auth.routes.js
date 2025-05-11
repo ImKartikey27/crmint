5a73f14d5966b0efac9d185c1de9c46a0102ebfa
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-12 01:37:25 +0530

    FEAT: check Auth

--- backend/api-server/routes/auth.routes.js
+++ backend/api-server/routes/auth.routes.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import passport from 'passport';
+import { isAuthenticated } from '../middlewares/Auth.middlewares.js';
 
 const router = express.Router();
+
+const checkAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.status(401).json({ message: "Unauthorized" });
+}
 
 router.route("/google").get(passport.authenticate("google",{
     scope: ["profile", "email"]
@@ -16,10 +24,15 @@ router.route("/google/callback").get(passport.authenticate("google",{
     }
 )
 
-router.route("/logout").get((req, res) => {
+router.route("/logout").get((req, res, next) => {
     req.logout();
-    res.redirect("http://localhost:5173/login");
+    res.redirect("http://localhost:5173/login")
 })
+
+// Check auth route
+router.route("/check").get(checkAuth, (req, res) => {
+    res.status(200).json({ message: "Authenticated", user: req.user });
+});
 
 
 export default router
