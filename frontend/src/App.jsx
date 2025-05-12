01b3f44b4daf26179a7d13660439c98eef3e6c2a
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-12 23:35:28 +0530

    FIX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,9 +19,9 @@ function App() {
           <Route
             path="/dashboard/*"
             element={
-              <ProtectedRoute>
+              
                 <DashboardLayout />
-              </ProtectedRoute>
+              
             }
           />
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
